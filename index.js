a8975793ba0f3895ee95acd1401c6cd0a29d1f19
Nija <nija.perry21@gmail.com>
2021-03-23 10:20:26 -0400

    commit tree 7a61cba5c205e5e3f5910afb98a7e868c9052259

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solutions in this file
+const names = ['Lisa', 'Kaitlin', 'Jan'];
+
+function writeCards(names, event) {
+  for (let i = 0; i < names.length; i++) {
+    return `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+  }
+  return names;
+}
+writeCards(names);
+
+
+
+
+// for (let age = 30; age < 40; age++) {
+//   console.log(`I'm ${age} years old.  Happy birthday to me!`);
+
+// }
+
+// const gifts = ["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//   for (let i = 0; i < gifts.length; i++) {
+//     console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//   }
+//   return gifts;
+// }
+// wrapGifts(gifts);
